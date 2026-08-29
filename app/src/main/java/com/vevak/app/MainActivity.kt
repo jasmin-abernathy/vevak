109c007dd7a1ef5497340fa3f78e2f590968ecf2
Jasmin-potager <jasmin@lepotager.org>
2026-08-29 09:46:09 +0200

    feat: switch app to beta UX root

--- app/src/main/java/com/vevak/app/MainActivity.kt
+++ app/src/main/java/com/vevak/app/MainActivity.kt
@@ -8,12 +8,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.vevak.app.ui.VeVakRoot
+import com.vevak.app.ui.VeVakBetaRoot
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { VeVakRoot() }
+        setContent { VeVakBetaRoot() }
     }
 }
