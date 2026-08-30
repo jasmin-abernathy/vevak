15b93ca396851f842cd9dd9266f61cbda23a5b55
Jasmin-potager <jasmin@lepotager.org>
2026-08-30 13:38:20 +0200

    Bump unified location beta to 0.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vevak.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.3.2"
+        versionCode = 6
+        versionName = "0.3.3"
 
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
