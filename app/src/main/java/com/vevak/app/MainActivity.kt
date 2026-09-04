c19a1fa752e8fb8b2fae2591f3ff00f841bc3ba3
Jasmin-potager <jasmin@lepotager.org>
2026-09-04 17:41:06 +0200

    Route setup through final hardening root

--- app/src/main/java/com/vevak/app/MainActivity.kt
+++ app/src/main/java/com/vevak/app/MainActivity.kt
@@ -8,12 +8,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.vevak.app.ui.VeVakAppRoot
+import com.vevak.app.ui.VeVakFinalRoot
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { VeVakAppRoot() }
+        setContent { VeVakFinalRoot() }
     }
 }
