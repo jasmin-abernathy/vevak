2023bc7a5b886ecfc5419fb2f903a4c1d4af31ec
Jasmin-potager <jasmin@lepotager.org>
2026-09-04 17:57:03 +0200

    Use cleaned stable setup root directly

--- app/src/main/java/com/vevak/app/MainActivity.kt
+++ app/src/main/java/com/vevak/app/MainActivity.kt
@@ -8,12 +8,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.vevak.app.ui.VeVakFinalRoot
+import com.vevak.app.ui.VeVakAppRoot
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { VeVakFinalRoot() }
+        setContent { VeVakAppRoot() }
     }
 }
