170d447c257492e6c46d5127aa5d2f5a72d69593
Jasmin-potager <jasmin@lepotager.org>
2026-09-04 11:27:41 +0200

    Bump VeVak beta to 0.3.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vevak.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.3.8"
+        versionCode = 12
+        versionName = "0.3.9"
 
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
